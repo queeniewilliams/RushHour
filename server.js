44c58a35b81d3cbf0bdbc0a9aaf9b2e0abeaf013
queeniewilliams <queeniepu329@gmail.com>
2021-04-19 08:51:17 -0700

    edited server

--- server.js
+++ server.js
@@ -15,15 +15,14 @@ app.use(logger('dev'))
 app.use(cors())
 app.use(bodyParser.json())
 
-app.use('/parking', ParkingRouter)
-app.use('/auth', AuthController)
-app.use('/comment', CommentController)
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')))
   app.get('*', (req, res) => {
     res.sendFile(path.join(`${__dirname}/client/build/index.html`))
   })
 }
+app.use('/parking', ParkingRouter)
+app.use('/auth', AuthController)
+app.use('/comment', CommentController)
 
 app.listen(PORT, () => console.log(`App Listening On Port: ${PORT}`))
